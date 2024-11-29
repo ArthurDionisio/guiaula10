ae27b4bb8bb57167c72e6e1a836cb03d27eaf852
ArthurDionisio <arthurdionisio.fellipe@gmail.com>
2024-11-29 10:01:46 -0300

    terceiro commit

--- escola/src/Paginas/Home.jsx
+++ escola/src/Paginas/Home.jsx
@@ -1,8 +1,36 @@
 export default function Home()
 {
     return(
-        <div>
-            <h1>Rotas</h1>
+        <div className="conteudo">
+            <h1>Aula 10 - Projeto Escola com MySQL</h1>
+
+<h2>0bjetivos da Aula: Conceitos de Frontend e Backend na Web</h2>
+<p>Nesta aula, abordaremos os principais conceitos e práticas relacionados ao desenvolvimento web, foca</p>
+
+<h2>Objetivos da Aula</h2>
+
+<h3>1. Introdução ao Desenvolvimento Web:</h3>
+<ul>
+
+    <li>Apresentar os conceitos de <strong>frontend</strong> e <strong>backend</strong> e explicar como</li>
+    <li>Definir a função de cada camada:
+<ul>
+    <li><strong>Frontend:</strong> Responsável pela interface do usuário e interação no navegac
+    <li><strong>Backend:</strong> Gerencia a lógica de negócios, manipula o banco de dados e re
+</ul>
+</li>
+</ul>
+Copie
+<h3>2. Navegação entre Páginas com React Router:</h3>
+<ul>
+<li>Configuração e uso do <strong>Router</strong> no React para permitir a navegação entre diferent
+<li >Criação de um componente que será o <strong>Menu</strong> de navagegação do site.</li>
+</li>Criação de links de navegação utilizando o componente <code>&lt;Link&gt;</code> do <code>react-
+<ul>
+<h3>3. Implementação de um CRUD Completo:</h3>
+<ul>
+<li><strong>Backend com Node.js:</strong>
+</ul>   
         </div>
     );
 }
